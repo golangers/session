38d6d792058c071c3c49ff2263d6dedf743e4e95
leetaifook <lee@leetaifook.com>
2013-02-22 11:54:58 +0800

    Update

--- doc.go
+++ doc.go
@@ -15,10 +15,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-Golanger Web Framework
+Golanger Session
 =======================================================================
 
-Golanger is a lightweight framework for writing web applications in Go. 
+Golanger is a lightweight framework for writing web applications in Go.
 
 */
 
